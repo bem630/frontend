4c117f0f13ed4327c3a3cdff5052feacbe4835b5
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-10-15 18:44:24 +0100

    ✨ F01: Add routing system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
